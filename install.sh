0d1fadea7f6c062a2424a9e4cfbace7a30c0ce63
mengshengzhi <mengshengzhi@baidu.com>
2016-02-01 20:28:18 +0800

    Add TERM autoconfig in install.sh
    Now only support zsh & bash. Users need to source rc files by self.
    
    Fix: #4
    
    Signed-off-by: mengshengzhi <mengshengzhi@baidu.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,34 @@
-cd ~/.vim/bundle/vimproc.vim/
+cd ~/vimrc/bundle/vimproc.vim/
 make
 cd ~
 unlink ~/.vimrc
-ln -s ~/.vim/vimrc ~/.vimrc
-echo "Install OK"
+ln -s ~/vimrc/vimrc ~/.vimrc
 
+if [ x$TERM != x"xterm-256color" ]
+then
+    echo "TERM is not xterm-256color, will set TERM=xterm-256color now"
+
+    is_zsh=`echo $SHELL|grep 'zsh'|wc -l`
+    is_bash=`echo $SHELL|grep 'bash'|wc -l`
+
+    if [ $is_zsh -eq 1 ]
+    then
+        echo "Your shell is zsh, set TERM in ~/.zshrc"
+        echo "export TERM=xterm-256color" >> ~/.zshrc
+        echo "Set TERM OK, please execute:"
+        echo "    source ~/.zshrc"
+    elif [ $is_bash -eq 1 ]
+    then
+        echo "Your shell is bash, set TERM in ~/.bash_profile"
+        echo "export TERM=xterm-256color" >> ~/.bash_profile
+        echo "Set TERM OK, please execute:"
+        echo "    source ~/.bash_profile"
+    else
+        echo "Your shell cannot set TERM automatically, please set TERM to xterm-256color by yourself"
+    fi
+
+else
+    echo "TERM is xterm-256color, OK"
+fi
+
+echo "Install OK"
