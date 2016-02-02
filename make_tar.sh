cf83eb58f18faa1bcbbde287a4fb8386925eb6c6
mengshengzhi <mengshengzhi@baidu.com>
2016-02-03 02:05:11 +0800

    Update make_tar.sh
    Signed-off-by: mengshengzhi <mengshengzhi@baidu.com>

--- make_tar.sh
+++ make_tar.sh
@@ -1,13 +1,14 @@
 cd ~
+vim_path=.vim
 backup_date=`date +%Y%m%d`
 echo $backup_date
 if [ x$1 != x"full" ]
 then
-    echo "tar --exclude .git -czvf vimrc-$backup_date-thin.tar.gz vimrc"
-    tar --exclude .git -czvf vimrc-$backup_date-mini.tar.gz vimrc
+    echo "tar --exclude .git -czvf vimrc-$backup_date-thin.tar.gz $vim_path"
+    tar --exclude .git -czvf vimrc-$backup_date-mini.tar.gz $vim_path
 else
-    echo "tar -czvf vimrc-$backup_date-all.tar.gz vimrc"
-    tar -czvf vimrc-$backup_date-full.tar.gz vimrc
+    echo "tar -czvf vimrc-$backup_date-all.tar.gz $vim_path"
+    tar -czvf vimrc-$backup_date-full.tar.gz $vim_path
 fi
 
 cd -
